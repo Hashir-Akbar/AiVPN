5fb361412a01157e9e85e17a32d2b22dedcc8205
azharnaeem76 <azharnaeem6679@gmail.com>
2025-02-09 21:23:02 +0500

    update

--- src/components/global/button.jsx
+++ src/components/global/button.jsx
@@ -7,7 +7,7 @@ export default function Button ({linkHref, btnContent, btnClass, stars}) {
     return(
         <>
             <button>
-                <Link href={linkHref} className={"bg-gradient-to-r from-[#4040FB] to-[#D220FF] inline-block   place-content-center px-6 py-2 font-geist font-medium leading-none" + btnClass+
+                <Link href={linkHref} target="_blank" className={"bg-gradient-to-r from-[#4040FB] to-[#D220FF] inline-block   place-content-center px-6 py-2 font-geist font-medium leading-none" + btnClass+
                     (stars && "flex gap-2 items-center justify-between pl-8")
 
 
--- src/components/global/header.jsx
+++ src/components/global/header.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Button from "@/components/global/button";
 
 
-export default function Header (props){
+export default function Header(props) {
 
     const navbar = [
         {
@@ -25,25 +25,33 @@ export default function Header (props){
 
     ]
 
-    return(
+    return (
         <>
             <header className={"border-b border-lightGrey"}>
-            <div className={" container    flex justify-between items-center"}>
-                <div className={"flex items-center "}>
-                <Image src={'logo.svg'} width="80" height="80" alt={"logo"} />
-                    <h2 className={"font-bold text-5xl font-poppins"}>AiVPN</h2>
+                <div className={" container    flex justify-between items-center"}>
+                    <div className={"flex items-center "}>
+                        <Image src={'logo.svg'} width="80" height="80" alt={"logo"} />
+                        <h2 className={"font-bold text-5xl font-poppins"}>AiVPN</h2>
+                    </div>
+                    <nav className={"flex items-center gap-12 font-dm-sans"}>
+                        {
+                            navbar.map((item, index) => (
+                                <Link key={index} href={item.href}>{item.nav_item}</Link>
+                            ))
+                        }
+                    </nav>
+                    <div>
+                        <Button
+                            linkHref={"https://chrome.google.com/webstore/detail/ai-vpn/ifledkgjbglbohmfdlfoefhcddgjbfge"}
+                            target={"_blank"}  // Open in new tab
+                            rel={"noopener noreferrer"}
+                            btnContent={"Download Extension"}
+                            btnClass={"py-3 px-10 border border-gray-300"}
+                        />
+
+                        {/* <Button linkHref={"/"} btnContent={"Connect  VPN"} btnClass={"py-3 px-10 border border-gray-300"}/> */}
+                    </div>
                 </div>
-                <nav className={"flex items-center gap-12 font-dm-sans"}>
-                    {
-                        navbar.map((item, index) => (
-                            <Link key={index} href={item.href}>{item.nav_item}</Link>
-                        ))
-                    }
-                </nav>
-                <div>
-                    <Button linkHref={"/"} btnContent={"Connect  VPN"} btnClass={"py-3 px-10 border border-gray-300"}/>
-                </div>
-            </div>
             </header>
         </>
     )
